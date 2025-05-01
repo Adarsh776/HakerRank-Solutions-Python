cd236cec81057b4ded7ba84efa4585ee07642e85
Adarsh <adarshadi7760@gmail.com>
2025-05-01 12:20:47 +0530

    Solved a problem on set symmetric difference

---
+++ Set-symmetricDifference.py
@@ -0,0 +1,6 @@
+num_stud_Eng=int(input())
+list_of_stud_Eng=set(map(int,input().split()))
+num_stud_Fren=int(input())
+list_of_stud_Fren=set(map(int,input().split()))
+
+print(len(list_of_stud_Eng ^ list_of_stud_Fren))
